e4c86f506547583fa91f3ae7889dd5d3e85e426b
Lena Hauer <lena.hauer@me.com>
2025-02-01 17:18:26 +0100

    Added CreatePost and PostDetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
+import NavBar from "./components/NavBar";
+
 //import Post from "./pages/Post";
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
     },
-    // {
-    //   path: "/post/:id",
-    //   element: <Post />,
-    // },
+    {
+      path: "/post/:id",
+      element: <PostDetails />,
+    },
+    {
+      path: "/post/create",
+      element: <CreatePost setEntries={setEntries} />,
+    },
   ]);
 
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}>
+        <NavBar />
+      </RouterProvider>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import harryPotterLogo from "../assets/harry-potter.svg";
-import EntryModal from "./EntryModal";
 
-const NavBar = ({ setEntries }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-
+const NavBar = () => {
   return (
     <nav className="flex items-center justify-between bg-white p-4 shadow-md">
       <Link to="/" className="flex items-center space-x-2">
         <img src={harryPotterLogo} alt="Harry Potter Logo" className="h-16" />
       </Link>
 
-      <button className="btn" onClick={() => setModalOpen(true)}>
-        Add entry
-      </button>
-
-      {modalOpen && (
-        <EntryModal
-          setEntries={setEntries}
-          closeModal={() => setModalOpen(false)}
-        />
-      )}
+      <Link
+        to={`/post/create`}
+        className="btn bg-white text-navy-800 border-none btn-primary"
+      >
+        Create Post
+      </Link>
     </nav>
   );
 };
